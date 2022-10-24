4448e8ca18e4c51a1774613d79f1fe2fb7d199e9
Pitzer34 <peter00880@gmail.com>
2022-10-24 19:03:21 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //將指定文字內容存檔, 由於要存放的文字內容與檔案存放位置都會
+            //變化, 所以我們可以將這兩項資訊設計成 method的參數, 由外界傳入
+            //請設計一個FileManager class, 內含一個 Save method
+            //只需要設定method, 不必實作出功能
+
+        }
+    }
+    class FileManager
+    {
+        public void SaveMethod(string word, string file)
+        {
+
+        }
+    }
+}
